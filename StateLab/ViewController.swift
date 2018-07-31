dca967735a542893dee272a5410cf952236acd77
klarheit <klarheit@klarheits-macbook-pro.local>
2018-08-01 00:06:57 +0900

    Initial Commit

---
+++ StateLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StateLab
+//
+//  Created by klarheit on 01/08/2018.
+//  Copyright © 2018 klarheit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
